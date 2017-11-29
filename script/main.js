221ac04336330f931eb82eaae897e6725d6356bf
jdrprod <jdrprod@gmail.com>
2017-11-29 21:21:16 +0100

    main files

---
+++ script/main.js
@@ -0,0 +1,17 @@
+var app = playground({
+
+    width: 600,
+    height : 400,
+    scale: 1.5,
+    create: function() {
+
+    },
+
+    step: function() {
+
+    },
+
+    render: function() {
+        this.layer.clear('#333');
+    }
+})
