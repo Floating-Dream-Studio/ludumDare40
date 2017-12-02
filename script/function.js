86f4896d406b676e73914051e7ec37e504aa0c76
jdrprod <jdrprod@gmail.com>
2017-12-02 16:00:34 +0100

    add dr function

--- script/function.js
+++ script/function.js
@@ -1,10 +1,5 @@
 // functions
-app.popupTimer = function() {
-    if(app.ptimer.x > app.width){
-        app.tween(app.ptimer)
-            .to({x: 500})
-    } else {
-        app.tween(app.ptimer)
-        .to({x: 650})
-    }
+function dr(obj, app) {
+    app.layer.fillStyle(obj.c);
+    app.layer.fillRect(obj.x, obj.y, obj.w, obj.h);
 }
--- script/main.js
+++ script/main.js
@@ -5,18 +5,28 @@ var app = playground({
     scale  : 1.5,
 
     create: function() {
+        this.mouse.lock();
         this.pnjTimer = {
             x: 300,
             y: 50,
             w: 50,
             h: 50,
-            g: 0
+            c: "cyan"
         }
         this.ptimer = {
             x: 500,
             y: 50,
             w: 50,
-            h: 50
+            h: 50,
+            c:"green"
+        }
+        this.player = {
+            x: 0,
+            y: 350,
+            w: 50,
+            h: 50,
+            c:"red",
+            g: 0,
         }
 
     },
@@ -30,7 +40,9 @@ var app = playground({
     },
 
     mousemove: function(e) {
-
+        if(e.x > 0 && e.x < this.width) {
+            app.player.x = e.x;
+        }
     },
 
     mousedown: function(e) {
@@ -42,13 +54,25 @@ var app = playground({
     },
 
     step: function() {
-
     },
 
     render: function() {
         this.layer.clear('#333');
         this.layer.fillStyle("red");
-        this.layer.fillRect(this.pnjTimer.x, this.pnjTimer.y, this.pnjTimer.w, this.pnjTimer.h);
-        this.layer.fillRect(this.ptimer.x, this.ptimer.y, this.ptimer.w, this.ptimer.h);
+        //this.layer.fillRect(this.ptimer.x, this.ptimer.y, this.ptimer.w, this.ptimer.h);
+        //this.layer.fillRect(this.pnjTimer.x, this.pnjTimer.y, this.pnjTimer.w, this.pnjTimer.h);
+        dr(this.pnjTimer, this);
+        dr(this.player, this);
+    },
+
+    //functions
+    popupTimer: function() {
+        if(app.ptimer.x > app.width){
+            app.tween(app.ptimer)
+                .to({x: 500})
+        } else {
+            app.tween(app.ptimer)
+                .to({x: 650})
+        }
     }
 })
